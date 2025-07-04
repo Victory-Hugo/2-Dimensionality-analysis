72f008ec49349a46a6a91aa9bbbbdbf9cbf46b4e
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-04 09:06:26 +0800

    1

--- 1-PLINK/script/0-core_SNP.sh
+++ 1-PLINK/script/0-core_SNP.sh
@@ -15,19 +15,23 @@
 '
 
 # 定义输入输出路径 
-INPUT_VCF="/mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/\output/merge/merged_biallelic_N_correct.vcf.gz"
-OUTPUT_PREFIX="/mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/output/merge/merged_biallelic_N_correct.filtered.maf0.01.miss0.99"
+INPUT_VCF="/mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/output/merge/merged_biallelic_N_correct_7544.vcf.gz"
+OUTPUT_PREFIX="/mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/output/merge/merged_biallelic_N_correct_7544.miss0.99"
 
 # 运行 VCFtools 进行过滤
 #! 去掉 MAF < 0.01 的位点(去掉超低频 SNP)
 #! 去掉缺失率 > 1% 的位点(去掉大范围缺失的位点)
+# vcftools --gzvcf $INPUT_VCF \
+#          --maf 0.01 \
+#          --max-missing 0.99 \
+#          --recode \
+#          --recode-INFO-all \
+#          --out $OUTPUT_PREFIX
 vcftools --gzvcf $INPUT_VCF \
-         --maf 0.01 \
          --max-missing 0.99 \
          --recode \
          --recode-INFO-all \
          --out $OUTPUT_PREFIX
-
 # 压缩和索引过滤后的 VCF
 bgzip ${OUTPUT_PREFIX}.recode.vcf
 tabix -p vcf ${OUTPUT_PREFIX}.recode.vcf.gz
