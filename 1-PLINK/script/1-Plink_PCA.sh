c7df513001f2188fa6de47a6737a74c4ffcd56a9
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-07-05 17:43:22 +0800

    1

--- 1-PLINK/script/1-Plink_PCA.sh
+++ 1-PLINK/script/1-Plink_PCA.sh
@@ -8,11 +8,11 @@
 set -e
 
 # 定义输入和输出路径
-INPUT_VCF="/mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/output/merge/merged_biallelic_7544_re.NoN.vcf.gz"
-OUTPUT_DIR="/mnt/d/幽门螺旋杆菌/Script/分析结果/3-PCA/7544_整体"
+INPUT_VCF="/mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/output/merge/merged_biallelic_2819.NoN.maf99.WGS.recode.vcf.gz"
+OUTPUT_DIR="/mnt/d/幽门螺旋杆菌/Script/分析结果/3-PCA/2819_hpEAsia_maf99"
 mkdir -p "$OUTPUT_DIR"
 PLINK_PREFIX="${OUTPUT_DIR}/filtered"
-PCA_OUTPUT_DIR="/mnt/d/幽门螺旋杆菌/Script/分析结果/3-PCA/7544_整体"
+PCA_OUTPUT_DIR="/mnt/d/幽门螺旋杆菌/Script/分析结果/3-PCA/2819_hpEAsia_maf99"
 mkdir -p "$PCA_OUTPUT_DIR"
 
 # 将 VCF 转换为 PLINK 二进制格式
