f052260014c82da891583f4c20b556a895990259
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-08-03 15:17:20 +0800

    2025年8月3日

--- 1-PLINK/script/1-Plink_PCA.sh
+++ 1-PLINK/script/1-Plink_PCA.sh
@@ -8,11 +8,11 @@
 set -e
 
 # 定义输入和输出路径
-INPUT_VCF="/mnt/d/幽门螺旋杆菌/Script/分析结果/1-序列处理流/output/merge_fasta/WGS.aln.fasta.vcf.gz"
-OUTPUT_DIR="/mnt/d/幽门螺旋杆菌/Script/分析结果/4-treemix/data/7544_WGS_aln/filtered"
+INPUT_VCF="/mnt/c/Users/Administrator/Desktop/cleaned.vcf.gz"
+OUTPUT_DIR="/mnt/c/Users/Administrator/Desktop/filtered"
 mkdir -p "$OUTPUT_DIR"
 PLINK_PREFIX="${OUTPUT_DIR}/filtered"
-PCA_OUTPUT_DIR="/mnt/d/幽门螺旋杆菌/Script/分析结果/4-treemix/data/7544_WGS_aln/filtered_PCA"
+PCA_OUTPUT_DIR="/mnt/c/Users/Administrator/Desktop/filtered_PCA"
 mkdir -p "$PCA_OUTPUT_DIR"
 
 # 将 VCF 转换为 PLINK 二进制格式
@@ -29,12 +29,12 @@ plink --bfile "$PLINK_PREFIX" \
 	--allow-extra-chr \
       --out "${PLINK_PREFIX}_ld_pruned"
 
-# # 运行主成分分析（PCA）
-# echo "运行主成分分析（PCA）"
-# plink --bfile "$PLINK_PREFIX" \
-#       --extract "${PLINK_PREFIX}_ld_pruned.prune.in" \
-#       --pca 20 \
-# 	  --allow-extra-chr \
-#       --out "${PCA_OUTPUT_DIR}/hpglobal_LD_PCA"
+# 运行主成分分析（PCA）
+echo "运行主成分分析（PCA）"
+plink --bfile "$PLINK_PREFIX" \
+      --extract "${PLINK_PREFIX}_ld_pruned.prune.in" \
+      --pca 20 \
+	  --allow-extra-chr \
+      --out "${PCA_OUTPUT_DIR}/hpglobal_LD_PCA"
 
 echo "PCA 分析完成！所有输出文件保存在 ${PCA_OUTPUT_DIR}/ 目录下。"
